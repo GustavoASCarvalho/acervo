72399fb566e2e571bfc00011900acbbc9a3f1239
GustavoASCarvalho <gustavoalexandrescarvalho@gmail.com>
2021-10-12 18:17:08 -0300

    Config database

--- config/database.ts
+++ config/database.ts
@@ -1,46 +1,25 @@
-/**
- * Config source: https://git.io/JesV9
- *
- * Feel free to let us know via PR, if you find something broken in this config
- * file.
- */
-
 import Env from '@ioc:Adonis/Core/Env'
+import Application from '@ioc:Adonis/Core/Application'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 
+import Url from 'url-parse'
+
+const databaseUrl = new Url(Env.get('DATABASE_URL'))
 const databaseConfig: DatabaseConfig = {
-  /*
-  |--------------------------------------------------------------------------
-  | Connection
-  |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
-  */
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
-    /*
-    |--------------------------------------------------------------------------
-    | PostgreSQL config
-    |--------------------------------------------------------------------------
-    |
-    | Configuration for PostgreSQL database. Make sure to install the driver
-    | from npm when using this connection
-    |
-    | npm i pg
-    |
-    */
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('PG_HOST'),
-        port: Env.get('PG_PORT'),
-        user: Env.get('PG_USER'),
-        password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        host: databaseUrl.hostname as string,
+        port: databaseUrl.port as number,
+        user: databaseUrl.username as string,
+        password: databaseUrl.password as string,
+        database: databaseUrl.pathname.substr(1) as string,
+        // ssl: {
+        //   rejectUnauthorized: false,
+        // },
       },
       migrations: {
         naturalSort: true,
@@ -48,8 +27,7 @@ const databaseConfig: DatabaseConfig = {
       healthCheck: false,
       debug: false,
     },
-
-  }
+  },
 }
 
 export default databaseConfig
